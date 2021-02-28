672a5e6e489f05c3e4ea953fbe4e62fa627d1186
Jozsi <miklo.jozsef.peter@student.ms.sapientia.ro>
2021-02-28 16:37:04 +0200

    Gyakorlat 2(2.feladat)

--- Lab2/gyak2.2/main.c
+++ Lab2/gyak2.2/main.c
@@ -10,3 +10,15 @@ int main() {
     }
     return 0;
 }
+/**
+ * Algoritmus elemzes-futasi ido
+ * Linearis kereses-linear_search           Koltseg             Vegrehajtasi szam
+ *for(int i=0;i<n;++i){                     c1                      n+1
+        if(*(array+i)==value){              c2                      n
+            return i;                       0                       1
+        }
+    }
+    return -1;                              0                       1
+    Osszesen:                               C                       2*n+3
+
+*/
