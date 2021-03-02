41475a775def87f2c219af70a362b129e37c25a4
Jozsi <miklo.jozsef.peter@student.ms.sapientia.ro>
2021-03-02 17:32:46 +0200

    Hazi-Gyakorlat 2.3

---
+++ Lab3/Lab2.3/cmake-build-debug/CMakeFiles/Lab2_3.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab2_3.dir/functions.c.obj"
+  "CMakeFiles/Lab2_3.dir/main.c.obj"
+  "Lab2_3.exe"
+  "Lab2_3.exe.manifest"
+  "Lab2_3.pdb"
+  "libLab2_3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab2_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Lab3/Lab2.3/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "functions.h"
+
+int main() {
+    int a,b,c;
+    scanf("%i%i%i",&a,&b,&c);
+    result(a,b,c);
+    return 0;
+}
