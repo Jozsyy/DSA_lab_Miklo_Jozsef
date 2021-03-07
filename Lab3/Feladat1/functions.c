af0a28f6867e4c8aa1ed06a83586405faa710c4b
Jozsi <miklo.jozsef.peter@student.ms.sapientia.ro>
2021-03-07 13:05:14 +0200

    Extra(nem mukodik)

--- Lab3/Feladat1/functions.c
+++ Lab3/Feladat1/functions.c
@@ -3,3 +3,56 @@
 //
 
 #include "functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+CEG* create(int n){
+    CEG *a=(CEG*)malloc(n*sizeof(CEG));
+    if(!a){
+        printf("Hiba");
+        return 0;
+    }
+    return a;
+}
+
+void read_data(CEG* a,int n,FILE *f){
+    for(int i=0;i<n;++i){
+        fscanf(f,"%s%s%c",a[i].vnev,a[i].knev,&a[i].vegzettseg[0]);
+        if(a[i].vegzettseg[0]=='K'){
+            fscanf(f,"%s%lf",a[i].kv.liceum,&a[i].kv.atlag);
+        }
+        else if(a[i].vegzettseg[0]=='F'){
+            fscanf(f,"%s%s%lf",a[i].ff.intezmeny,a[i].ff.kar,&a[i].ff.atlag);
+        }
+    }
+}
+
+void print_data(CEG *a, int n) {
+    printf("Vegzettseg nelkuliek:\n");
+    for(int i=0;i<n;++i){
+            printf("%s %s\n", a[i].vnev,a[i].knev);
+    }
+    for(int i=0;i<n;++i){
+        if(a[i].vegzettseg[0]=='N') {
+            printf("%s %s\n", a[i].vnev,a[i].knev);
+        }
+    }
+    printf("Kozepfoku vegzettsegesek:\n");
+    for(int i=0;i<n;++i){
+        if(a[i].vegzettseg[0]=='K') {
+            printf("%s %s: %s %lf\n", a[i].vnev,a[i].knev, a[i].kv.liceum, a[i].kv.atlag);
+        }
+    }
+    printf("Felsofoku vegzettsegesek:\n");
+    for(int i=0;i<n;++i){
+        if(a[i].vegzettseg[0]=='F') {
+            printf("%s %s: %s %s %lf\n", a[i].vnev,a[i].knev, a[i].ff.intezmeny, a[i].ff.kar,a[i].ff.atlag);
+        }
+    }
+
+}
+
+void destroy(CEG *a){
+    free(a);
+}
--- Lab3/Feladat1/main.c
+++ Lab3/Feladat1/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include "functions.h"
+
 
 int main() {
-    printf("Hello, World!\n");
+    FILE *f=fopen("be.txt","r");
+    CEG *a;
+    int n=2;
+    a=create(n);
+    read_data(a,n,f);
+    print_data(a,n);
+    destroy(a);
     return 0;
 }
