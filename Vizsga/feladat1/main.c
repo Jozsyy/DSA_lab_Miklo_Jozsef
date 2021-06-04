3291e04cd34c9260fc08b918a0b17f45c8f22799
Jozsi <miklo.jozsef.peter@student.ms.sapientia.ro>
2021-06-04 10:38:03 +0300

    Vizsga_feladatok

--- Vizsga/feladat1/main.c
+++ Vizsga/feladat1/main.c
@@ -1,6 +1,13 @@
 #include "stack.h"
 #include "heap.h"
 
+
+/** SZEMELYI SZAM
+ * 1 szamjegy ha 1 vagy 2-vel kezdodik akkor 1900-as ev, ha 5/6 akkor 2000-es ev
+ * 2-3 szamjegy a megadja a szuletesi ev veget pl:97  = 1997
+ * a tobbi nem lenyeges
+ */
+
 int year=2021;
 int main() {
     STACK *verem=create(10);        ///letrehozunk egy vermet
@@ -14,7 +21,7 @@ int main() {
             ++c;
         }
     }
-    printf("%i-en szulettek 2000 elott\n",c);
+    printf("%i-en/-an szulettek 2000 elott\n",c);
     kupacrendez(h,h->size);           ///a kupacot atrendezzuk, hogy a legkisebb elem legyen a kupac elso eleme
     int old=year-h->data[1];          ///kiszamitjuk a legidosebb szemely eletkorat majd kiirjuk
     printf("A legidosebb szemely eletkora: %i",old);
